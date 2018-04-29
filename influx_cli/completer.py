fdf7e47f219f8380b70d06f2ff9e56e499bd99e5
Stephen Chen <g1222888@gmail.com>
2018-04-29 15:46:19 +0800

    function name autocomplete

--- influx_cli/completer.py
+++ influx_cli/completer.py
@@ -1,10 +1,15 @@
 from prompt_toolkit.completion import Completer, Completion
 from fuzzyfinder import fuzzyfinder
 
-from .completion import KEYWORDS
+from .completion import KEYWORDS, FUNCTIONS
 
 class InfluxCompleter(Completer):
     def get_completions(self, document, complete_event):
         word = document.get_word_before_cursor(WORD=True)
-        suggestions = fuzzyfinder(word, KEYWORDS)
-        return [Completion(s, -len(word), display_meta='keyword') for s in suggestions]
+
+        suggestions = fuzzyfinder(word, KEYWORDS + FUNCTIONS)
+        for s in suggestions:
+            if s in KEYWORDS:
+                yield Completion(s, -len(word), display_meta='keyword')
+            elif s in FUNCTIONS:
+                yield Completion(s, -len(word), display_meta='function')
