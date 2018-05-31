5bff7257070104ebdb877f403fcbaedbcbb4b525
Stephen Chen <g1222888@gmail.com>
2018-06-01 00:34:40 +0800

    mock influx_client in test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,10 +10,19 @@ class MockCLI(object):
     def run(self):
         raise EOFError
 
+class MockClient(object):
+    def __init__(self, args=None):
+        pass
 
+    def ping(self):
+        pass
+
+
+@mock.patch('influx_prompt.main.Client', side_effect=MockClient)
 @mock.patch('influx_prompt.main.print_tokens', side_effect=None)
 @mock.patch('influx_prompt.main.CommandLineInterface', side_effect=MockCLI)
-def test_main(mock_cli, mock_print_tokens):
+def test_main(mock_cli, mock_print_tokens, mock_client):
     cli()
     mock_cli.assert_called()
     mock_print_tokens.assert_called()
+    mock_client.assert_called()
